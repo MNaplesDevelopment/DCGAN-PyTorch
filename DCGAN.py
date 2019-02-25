e9027f96a10e56ca98361093c1cdd80eb28c2c3a
Michael Naples <naplesthedev@gmail.com>
2019-02-24 21:55:10 -0500

    Update DCGAN.py

--- DCGAN.py
+++ DCGAN.py
@@ -147,7 +147,6 @@ def train(epochs):
 
             D_output_fake = netD(G_output).squeeze()    # prediction of the 32 generated images
             D_fake_loss = criterion(D_output_fake, y_fake)  # Binary Cross Entropy loss
-            D_fake_score = D_output_fake.data.mean()
 
             D_train_loss = D_fake_loss + D_real_loss
             D_train_loss.backward()
@@ -164,7 +163,8 @@ def train(epochs):
             optimizerG.step()
 
             if x % 50 == 0:
-                print('D loss: {}\tG loss: {}\tProgress: {}/{}'.format(D_train_loss, G_train_loss, x, 1875))
+                print('D loss: {}\tG loss: {}\tProgress: {}/{}'.format(D_train_loss, G_train_loss, x, 
+                                                                       len(train_loader)/batch_size))
 
             x += 1
 
@@ -262,73 +262,8 @@ def plot_images(grayscale):
     plt.show()
 
 
-#plot_images(False)
-#plot_layers(False)
-
-
-def Board():
-    _, img = netG.forward(create_noise(1))
-    img = img.detach().cpu().numpy()[0][0]
-    img = ((img - img.min()) * (1/img.max() - img.min()) * 255)
-
-    def create_vector():
-        arr = np.empty((1, 128, 1, 1))
-        for i in range(128):
-            arr[0][i][0][0] = sliders[i].get()
-        ten = torch.tensor(arr)
-        return ten.float()
-
-    def update_picture(x):
-        _, img = netG.forward(create_vector())
-        img = img.detach().cpu().numpy()[0]
-        img = ((img - img.min()) * (1 / img.max() - img.min()) * 50).astype('uint8')
-        img = img.transpose((1, 2, 0))
-        photo = ImageTk.PhotoImage(image=Image.fromarray(img, 'RGB').resize((256, 256), Image.ANTIALIAS))
-
-        label.configure(image=photo)
-        label.image = photo
-
-    def interpolate():
-        ndx = random.randint(0, 123)
-        direction = []
-        arr = [-1, 1]
-        for i in range(5):
-            direction.append(arr[random.randint(0, 1)])
-        for i in range(10):
-            for x in range(5):
-                sliders[ndx].set(sliders[ndx].get() + (direction[x] * 0.03))
-                ndx += 1
-            update_picture(1)
-            ndx -= 5
-            time.sleep(0.05)
-
-    def randomize():
-        for x in sliders:
-            x.set(random.uniform(-1, 1))
-
-    def reset():
-        for x in sliders:
-            x.set(0)
-
-    tk = Tk()
-
-    photo = ImageTk.PhotoImage(image=Image.fromarray(img))
-    label = Label(image=photo, height=256, width=256)
-    label.grid(row=0, column=0)
-
-    sliders = []
-    for i in range(128):
-        sliders.append(Scale(tk, from_=1, to=-1, resolution=0.01, command=update_picture))
-        sliders[i].grid(row=int(i/32), column=i % 32 + 2)
-
-    create = Button(text="interpolate", command=interpolate).grid(row=1, column=0)
-    rand = Button(text='randomize', command=randomize).grid(row=2, column=0)
-    zero = Button(text='reset', command=reset).grid(row=3, column=0)
-
-    tk.mainloop()
-
-
-#Board()
+# plot_images(False)
+# plot_layers(False)
 
 
 def interpolate(speed):
@@ -376,4 +311,6 @@ def interpolate(speed):
     imageio.mimsave('test.gif', images)
 
 
-#interpolate(10)
+# interpolate(10)
+
+
